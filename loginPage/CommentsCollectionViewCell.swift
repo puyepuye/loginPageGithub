a7c90e768fc4d57dbe677b410c2f5a6426439650
Sataphon Obra <puye53991@gmail.com>
2022-09-27 08:41:42 +0700

    push per 08:41

---
+++ loginPage/CommentsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CommentsCollectionViewCell.swift
+//  loginPage
+//
+//  Created by KMIDS on 27/9/2565 BE.
+//
+
+import UIKit
+
+class CommentsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ loginPage/CommentsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CommentsViewController.swift
+//  loginPage
+//
+//  Created by KMIDS on 26/9/2565 BE.
+//
+
+import UIKit
+
+class CommentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
